072391cb57b55e7c90fded6c01253549d3283543
Ishita Paul <119348118+ishita-5062@users.noreply.github.com>
2023-01-24 13:47:22 +0530

    Submitting Q4 of Assignment 2

---
+++ a2_q5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n=3;
+    for (int i=1; i<=n; i++){
+        for (int k=n-i; k>0; k--){
+            cout<<"   ";        }
+        for (int j=1; j<=2*i-1; j++){
+            cout<<"*  ";        }
+        cout<<endl;
+        
+    }
+    for (int i=n-1; i>=1; i--){
+        for (int k=1; k<=n-i; k++){
+            cout<<"   ";        }
+        for (int j=n-2*i; j<=1; j++){
+            cout<<"*  ";        }
+        cout<<endl;
+        
+    }
+}
